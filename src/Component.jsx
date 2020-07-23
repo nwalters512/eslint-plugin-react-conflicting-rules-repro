998123bbbf755bd4975ad0e3981cc941b52bae96
Nathan Walters <nwalters@nerdwallet.com>
2020-07-22 19:56:30 -0700

    Add repro

---
+++ src/Component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Component = () => {
+  return <span>{"/*"}</span>;
+};
+
+const Component2 = () => {
+  return <span>/*</span>;
+};
+
+export { Component, Component2 };
